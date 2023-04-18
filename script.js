6894eae37b450f7557a3b0258bc618157c3ff939
Fiona-alice <120317574+Fiona-alice@users.noreply.github.com>
2023-04-18 14:55:32 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function decimalToBinary(decimal) {
-  let binary = "";
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-  return binary || "0";
+function decimalToBinary(num){
+    let binaryStr = ""
+    while(num>0){
+        let rem = num%2;
+        binaryStr = binaryStr + rem.toString()
+ 
+        num = Math.floor(num/2)
+    }
+  
+    return binaryStr.split("").reverse().join("")
 }
+ 
+module.exports = decimalToBinary;
